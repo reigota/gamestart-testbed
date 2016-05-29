ef03f410cb49960d66384f647efbbc4aecd1c6b8
Tony <tonysaltodev@gmail.com>
2016-05-28 23:53:04 -0300

    Iniciando validações para movimentação hack and slash

---
+++ Assets/_Validations/Scripts/Player/Movement/PlayerHackSlashController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//Inicio da validação de movimentação hack and slash Tony.
+
+public class PlayerHackSlashController : MonoBehaviour 
+{
+    //Private variables
+    private Rigidbody rigidBody;
+    private Vector3 playerInput = Vector3.zero;
+
+    //Plubic delegates
+    public Action OnPlayerMovementEvent;
+    public Action OnPlayerStopEvent;
+    public Action OnPlayerJumpEvent;
+    public Action OnPlayerRunEvent;
+
+    //Inspector variables
+    [SerializeField] private float movementSpeed;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        CheckInputs();
+        MovimentPlayer();
+    }
+    
+    private void CheckInputs()
+    {
+       float verticalInput = Input.GetAxis("Vertical");
+       float horizontalInput = Input.GetAxis("Horizontal");
+
+       playerInput = new Vector3(horizontalInput, 0, verticalInput);
+    }
+
+    private void MovimentPlayer()
+    {
+        rigidBody.velocity = playerInput * movementSpeed;
+
+        if (playerInput != Vector3.zero)
+            DispatchPlayerMovementEvent();      
+    }
+
+    private void CheckJump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            DispatchPlayerJumpEvent();
+        }
+    }
+
+    private void CheckRun()
+    {
+        if (Input.GetButtonDown("Run"))
+        {
+            DispatchPlayerRunEvent();
+        }
+    }
+
+    //Dispatchers
+
+    private void DispatchPlayerMovementEvent()
+    {
+        if (OnPlayerMovementEvent != null)
+            OnPlayerMovementEvent();
+    }
+
+    private void DispatchPlayerStopEvent()
+    {
+        if (OnPlayerMovementEvent != null)
+            OnPlayerMovementEvent();
+    }
+
+    private void DispatchPlayerJumpEvent()
+    {
+        if (OnPlayerMovementEvent != null)
+            OnPlayerMovementEvent();
+    }
+
+    private void DispatchPlayerRunEvent()
+    {
+        if (OnPlayerMovementEvent != null)
+            OnPlayerMovementEvent();
+    }
+
+
+}
---
+++ Assets/_Validations/Scripts/Utils/TestEventSubscribers.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEventSubscribers : MonoBehaviour 
+{
+    [SerializeField]
+    private PlayerHackSlashController playerController;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerHackSlashController>();
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Subsc");
+        playerController.OnPlayerMovementEvent += OnPlayerMovement;
+    }
+    private void OnDisable()
+    {
+        Debug.Log("Unsubs");
+        playerController.OnPlayerMovementEvent -= OnPlayerMovement;
+    }
+
+    private void OnPlayerMovement()
+    {
+        Debug.Log("Player move");
+    }
+
+    private void OnPlayerStop()
+    {
+        Debug.Log("Player idle");
+    }
+
+}
