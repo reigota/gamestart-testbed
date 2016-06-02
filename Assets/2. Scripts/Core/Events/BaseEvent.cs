6e47d8d833f6ad0f03f7d6afdd3d599e39d66939
reigota <reigota@gmail.com>
2016-06-02 12:00:39 -0300

    BaseBehaviour, BaseEvent, JumpEvent, PlayerMovimentController, some interfaces... really, a bunch of infrastructure things..

---
+++ Assets/2. Scripts/Core/Events/BaseEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace Game.Core.Events
+{
+    public abstract class BaseEvent : UnityEvent, IEvent
+    {
+        public abstract string GetEventName();
+
+        public virtual void Dispatch()
+        {
+            Invoke();
+        }
+
+    }
+
+    public abstract class BaseEvent<T> : UnityEvent<T>, IEvent
+    {
+        public abstract string GetEventName();
+
+        public virtual void Dispatch()
+        {
+            throw new ArgumentNullException("arg", "Generic argument can not be omitted. Use BaseEvent instead of BaseEvent<T> as base class.");
+        }
+
+        public virtual void Dispatch(T arg)
+        {
+            Invoke(arg);
+        }
+
+    }
+}
---
+++ Assets/2. Scripts/Core/Events/PlayerEvents/JumpEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Game.Core.Events.PlayerEvents
+{
+    public enum JumpEvents
+    {
+        OnStartJump,
+        OnStartGoingDown,
+        OnStartFalling,
+        OnEndJump,
+        OnReachTheGround
+    }
+
+    [Serializable]
+    public class JumpEvent : BaseEvent<JumpEvents>, IEvent
+    {
+        public override string GetEventName() { return GetType().Name; }
+    }
+}
