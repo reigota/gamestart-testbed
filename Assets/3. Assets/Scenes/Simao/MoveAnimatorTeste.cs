39cc3589a8ac2f074bdba701460fe288327d7b8e
Simão Menezes <simaomenezesneto@gmail.com>
2016-06-16 22:03:31 -0300

    Adicionando animação de pulo

--- Assets/3. Assets/Scenes/Simao/MoveAnimatorTeste.cs
+++ Assets/3. Assets/Scenes/Simao/MoveAnimatorTeste.cs
@@ -4,16 +4,23 @@ using System.Collections;
 public class MoveAnimatorTeste : MonoBehaviour
 {
 
+    CapsuleCollider coliderCapsule;
+    Rigidbody _rigidbody;
 
     [SerializeField] private bool run =  false;
 
+    float tamanhoCapsule = 1.8f;
+    float tamanhoCOmPulo = 1.1471f;
     float hor = 0f;
     float ver = 0f;
     Animator anim;
+    private bool jump = false;
 
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        coliderCapsule = GetComponent<CapsuleCollider>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -28,6 +35,19 @@ public class MoveAnimatorTeste : MonoBehaviour
         anim.SetFloat("HorSpeed", hor, 0.2f, Time.deltaTime);
 
 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            coliderCapsule.height = tamanhoCOmPulo;
+            jump = true;
+
+            anim.SetBool("Jump", jump);
+            _rigidbody.AddForce(Vector3.up * .1f, ForceMode.Impulse);
+        } else
+        {
+            coliderCapsule.height = tamanhoCapsule;
+            jump = false;
+            anim.SetBool("Jump", jump);
+        }
 
 
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
---
+++ Assets/3. Assets/Scenes/Simao/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    Animator anim;
+    float hor = 0f;
+    float ver = 0f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+
+        anim.SetFloat("Forward", ver, 0.2f, Time.deltaTime);
+        anim.SetFloat("Turn", hor, 0.2f, Time.deltaTime);
+
+    }
+}
