6bbd133d5947401799de4c0f7143282d372ca3a1
Simão Menezes <simaomenezesneto@gmail.com>
2016-06-23 22:58:59 -0300

    Removendo arquivo não utilizado, removendo animator não utilizado

---
+++ Assets/3. Assets/Scenes/Simao/Scripts/LocomotionPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocomotionPlayer : MonoBehaviour
+{
+    #region Variables (private)
+    [SerializeField] private Animator anim;
+    [SerializeField] private float directionDampTime = .25f;
+
+    private float speed = 0.0f;
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+    #endregion
+
+    #region Properties (public)
+    #endregion
+
+    #region Unity event functions
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        if (anim.layerCount >= 2)
+        {
+            anim.SetLayerWeight(1, 1);
+        }
+    }
+
+    void Start () {
+	
+	}
+
+	void Update ()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        speed = new Vector3(horizontal, vertical).sqrMagnitude;
+
+        anim.SetFloat("Speed", speed);
+        anim.SetFloat("Direction", horizontal, directionDampTime, Time.deltaTime);
+	}
+    #endregion
+
+    #region Methods
+
+    #endregion Methods
+}
